442eff887fa2ef6620698b2ddca227f6e17c0009
Alex <Alex@Alex-PC>
2016-02-02 15:39:47 -0500

    Added fields and getter methods to Currency class

--- src/lab2b_team2/Currency.java
+++ src/lab2b_team2/Currency.java
@@ -9,6 +9,68 @@ package lab2b_team2;
  *
  * @author Alex
  */
+
 public class Currency {
     
+    // coins
+    final private double PENNY = 0.01;
+    final private double NICKEL = 0.05;
+    final private double DIME = 0.10;
+    final private double QUARTER = 0.25;
+    //bills (Clinton, Cosby, Nye etc.)
+    final private double ONE = 1.00;
+    final private double TEN = 10.00;
+    final private double TWENTY = 20.00;
+    final private double FIFTY = 50.00;
+    final private double HUNDRED = 100.00;
+    
+    public Currency ()
+    {
+        
+    }
+    
+    public double getPenny()
+    {
+    return PENNY;
+    }
+    
+    public double getNickel()
+    {
+    return NICKEL;
+    }
+    
+    public double getDime()
+    {
+    return DIME;
+    }
+    
+    public double getQuarter()
+    {
+    return QUARTER;
+    }
+    
+    public double getOne()
+    {
+    return ONE;
+    }
+    
+    public double getTen()
+    {
+    return TEN;
+    }
+    
+    public double getTwenty()
+    {
+    return TWENTY;
+    }
+    
+    public double getFifty()
+    {
+    return FIFTY;
+    }
+    
+    public double getHundred()
+    {
+    return HUNDRED;
+    }
 }
