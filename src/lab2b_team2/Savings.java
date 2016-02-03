da975c7d6aa0e744fa59d60e97611b214ea97cc2
kjrya <kjrya@130.203.98.52>
2016-02-02 19:22:09 -0500

    Savings is finished

--- src/lab2b_team2/Savings.java
+++ src/lab2b_team2/Savings.java
@@ -9,6 +9,46 @@ package lab2b_team2;
  *
  * @author Alex
  */
-public class Savings {
+public class Savings extends Accounts{
+    
+    public double interest;
+    
+    public Savings(double balance, int accountNumber, int pin, Customer customer, double interest){
+        super(balance, accountNumber, pin, customer);
+        this.interest = interest;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
+
+    @Override
+    public void depositCurrency(double amount) {
+        balance = balance + amount;
+    }
+    
+    public void compoundInterest(){
+        balance = balance*(1+interest);
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    @Override
+    public int getPIN() {
+        return pin;
+    }
+    
+    public double getInterest(){
+        return interest;
+    }
     
 }
