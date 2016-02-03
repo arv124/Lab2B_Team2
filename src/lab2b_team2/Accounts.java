1b10c17061fee624427809eaf673b240fdb3925b
kjrya <kjrya@130.203.98.52>
2016-02-02 19:21:23 -0500

    Accounts is finished

--- src/lab2b_team2/Accounts.java
+++ src/lab2b_team2/Accounts.java
@@ -9,6 +9,28 @@ package lab2b_team2;
  *
  * @author Alex
  */
-public class Accounts {
+public abstract class Accounts {
+    
+    protected double balance = 0;
+    protected int accountNumber;
+    protected int pin;
+    protected Customer customer;
+    
+    public Accounts(double balance, int accountNumber, int pin, Customer customer){ 
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.customer = customer;
+    }
+    
+    protected abstract void withdraw(double amount);
+    
+    protected abstract void depositCurrency(double amount);
+    
+    protected abstract double getBalance();
+    
+    protected abstract int getAccountNumber();
+    
+    protected abstract int getPIN();
     
 }
