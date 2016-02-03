4a141056ab6bbc32a0ef88ce2d93b6ca78309b2e
kjrya <kjrya@130.203.98.52>
2016-02-03 15:43:19 -0500

    Fixed the vend method

--- src/lab2b_team2/Currency.java
+++ src/lab2b_team2/Currency.java
@@ -79,4 +79,41 @@ public class Currency {
     {
     return HUNDRED;
     }
+    
+        //bills (Clinton, Cosby, Nye etc.)
+    public String vendBill(double amountVending){
+        double one = (amountVending%5);
+        double five = (amountVending%5);
+        double ten = (amountVending%10);
+        double twenty = (amountVending%20);
+        double fifty = (amountVending%50);
+        double hundred = (amountVending%100);
+        
+        if(amountVending % 10 == 0){
+            if(amountVending % 100 != 0){
+                amountVending = amountVending - ((amountVending%100)*100);
+            }
+            if(amountVending % 50 != 0){
+                
+                amountVending = amountVending - ((amountVending%50)*50);
+            }
+            if(amountVending % 20 != 0){
+                
+                amountVending = amountVending - ((amountVending%20)*20);
+            }
+            if(amountVending % 10 != 0){
+                
+                amountVending = amountVending - ((amountVending%10)*10);
+            }
+            if(amountVending % 5 != 0){
+                five = (amountVending%5);
+                amountVending = amountVending - ((amountVending%5)*5);
+            }
+            if(amountVending % 1 != 0){
+                amountVending = amountVending - ((amountVending%1)*1);
+            }
+        }
+        String str = "Hundred: "+hundred+"\nFifty: "+fifty+"\nTwenty: "+twenty+"\nTen: "+ten+"\nFive: "+five+"\nOne: "+one;
+        return str;
+    }
 }
