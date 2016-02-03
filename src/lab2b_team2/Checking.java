cbe243a0e8b4b855ba17428c09f0112a638daafd
kjrya <kjrya@130.203.98.52>
2016-02-02 19:21:53 -0500

    Checking is finished

--- src/lab2b_team2/Checking.java
+++ src/lab2b_team2/Checking.java
@@ -9,6 +9,43 @@ package lab2b_team2;
  *
  * @author Alex
  */
-public class Checking {
+public class Checking extends Accounts{
+    
+    public Checking(double balance, int accountNumber, int pin, Customer customer){
+        super(balance, accountNumber, pin, customer);
+    }
+    
+    @Override
+    public void withdraw(double amount){
+        balance = balance - amount;
+    }
+    
+    @Override
+    public void depositCurrency(double amount){
+        balance = balance + amount;
+    }
+    
+    public void depositCheck(Checks c){
+        balance = balance + c.balance;
+    }
+    
+    @Override
+    public double getBalance(){
+        return balance;
+    }
+    
+    @Override
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    
+    @Override
+    public int getPIN(){
+        return pin;
+    }
+    
+    public Customer getCustomer(){
+        return customer;
+    }
     
 }
